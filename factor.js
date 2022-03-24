f8be95afbf3604be5976ca6a0ef7c171d44b27f3
fmcs39 <102323517+fmcs39@users.noreply.github.com>
2022-03-24 21:26:05 +0200

    Add files via upload

---
+++ factor.js
@@ -0,0 +1,10 @@
+alert("welcome ")
+function fac(form) {
+    let x = document.getElementById('frestnam').value,
+        i,y=1;
+    for(i = x; i>=1;i--) {
+        y= y*i;
+    }
+    alert(y)
+}
+
